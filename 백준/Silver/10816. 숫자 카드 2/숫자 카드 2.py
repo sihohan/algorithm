424df0e43ff76af05d4359078f91109d5938a756
Sean Han <siho.sean.han@gmail.com>
2023-10-06 22:53:51 +0900

    Update 숫자 카드 2.py
    Correct spelling error

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,12 +1,12 @@
 import bisect
 
 input()
-serach_from = sorted(list(map(int, input().split())))
+search_from = sorted(list(map(int, input().split())))
 input()
 to_find = list(map(int, input().split()))
 
 ans = []
 for x in to_find:
-    ans.append(bisect.bisect_right(serach_from, x) - bisect.bisect_left(serach_from, x))
+    ans.append(bisect.bisect_right(search_from, x) - bisect.bisect_left(search_from, x))
 
 print(*ans)
