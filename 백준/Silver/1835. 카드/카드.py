d57b0fd3e3e950202f5259926af65865f0a85817
Sean Han <siho.sean.han@gmail.com>
2023-10-25 00:01:28 +0900

    [Silver IV] Title: 카드, Time: 104 ms, Memory: 34008 KB -BaekjoonHub

--- 백준/Silver/1835. 카드/카드.py
+++ 백준/Silver/1835. 카드/카드.py
@@ -3,19 +3,12 @@ from collections import deque
 n = int(input())
 dq = deque()
 
-# dq.append(3)
-# print(dq)
-# dq.append(4)
-# print(dq)
-# dq.appendleft(2)
-# print(dq)  # deque([2, 3, 4])
-
 for i in reversed(range(1, n + 1)):
     dq.appendleft(i)
     if i == n:
         continue
 
-    for _ in range(i):  # 0, 1, 2
+    for _ in range(i):
         dq.appendleft(dq.pop())
 
 print(*dq)
