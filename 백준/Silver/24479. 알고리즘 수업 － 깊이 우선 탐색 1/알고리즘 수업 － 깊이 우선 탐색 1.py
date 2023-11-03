ead484136115a8a0c5c266b3ef48dc9963b9e867
Sean Han <siho.sean.han@gmail.com>
2023-11-04 01:35:46 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 804 ms, Memory: 89104 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
@@ -0,0 +1,30 @@
+import sys
+
+sys.setrecursionlimit(10**7)
+input = sys.stdin.readline
+
+
+def dfs(adj, r, visited, vertex_to_order):
+    global order
+    visited[r] = True
+    order += 1
+    vertex_to_order[r] = order
+    # print(vertex_to_order)
+    for j in sorted(adj[r]):
+        if not visited[j]:
+            dfs(adj, j, visited, vertex_to_order)
+
+
+n, m, r = map(int, input().split())
+adj = [[] for _ in range(n + 1)]
+for _ in range(m):
+    i, j = map(int, input().split())
+    adj[i].append(j)
+    adj[j].append(i)
+
+visited = [False] * (len(adj))
+vertex_to_order = {vertex: 0 for vertex in range(1, n + 1)}
+order = 0
+dfs(adj, r, visited, vertex_to_order)
+for _, v in sorted(vertex_to_order.items()):
+    print(v)
