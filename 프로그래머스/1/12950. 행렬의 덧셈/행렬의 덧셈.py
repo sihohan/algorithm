c27b4910d09d007e6f0292a1a21645e790a58e88
Sean Han <siho.sean.han@gmail.com>
2023-12-20 00:48:38 +0900

    [level 1] Title: 행렬의 덧셈, Time: 12.94 ms, Memory: 22.7 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.py
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.py
@@ -1,7 +1,2 @@
 def solution(arr1, arr2):
-    res = []
-    for i, (row1, row2) in enumerate(zip(arr1, arr2)):
-        res.append([])
-        for n1, n2 in zip(row1, row2):
-            res[i].append(n1 + n2)
-    return res
+    return [[n1 + n2 for n1, n2 in zip(row1, row2)] for row1, row2 in zip(arr1, arr2)]
